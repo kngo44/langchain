382fba868462150d6162079f9710118a39559663
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-07-24 19:58:43 -0500

    added prompts and chains

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from langchain.llms import openai
-from dotenv import load_dotenv\
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_project_name():
+def generate_project_name(project_description):
     llm = openai(temperature=0.7)
 
-    name = llm("I am developing a website that creates personalized mock interviews and I want a cool name for it. Suggest me five cool names for my project.")
+    prompt_template_name = PromptTemplate(
+        input_variables=['project_description'],
+        template="I am developing a website that {project_description} and I want a cool name for it. Suggest me five cool names for my project."
+    )
 
-    return name
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
 
+    response = name_chain({'project_description': project_description})
+    return response
+
+if __name__ == "__main__":
+    print (generate_project_name("generates personalized mock interviews"))
