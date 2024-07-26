7d5a6ba8a4dd5305dd0ac464765150c04fbd225e
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-07-26 17:59:04 -0500

    Added vector db

---
+++ assistant.py
@@ -0,0 +1,19 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import retrieval_qa
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+def yt_vectordb(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
